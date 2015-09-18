235cc3a7529b36e11a7935e15c90f496210d7c31
Gustavo Machado <gdmachado@me.com>
2015-09-18 17:30:15 -0300

    implement method for generating request signature

---
+++ scup/auth.py
@@ -0,0 +1,11 @@
+import hashlib
+import time
+
+def get_request_signature(private_key):
+	current_time = int(time.time())
+
+	message = '{}{}'.format(current_time, private_key)
+
+	digest = hashlib.md5(message).hexdigest()
+
+	return current_time, digest
