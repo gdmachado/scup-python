e6699947ebde4d51b1bd8b6016879d4917d7a648
Gustavo Machado <gdmachado@me.com>
2015-09-18 17:28:33 -0300

    implement initial base exception class and httperror exception class

---
+++ scup/exceptions.py
@@ -0,0 +1,18 @@
+class ScupPythonError(Exception):
+	""" Base class for exceptions raised by scup-python. """
+
+
+class ScupError(ScupPythonError):
+	""" Exception for Scup errors. """
+	def __init__(self, message=None, code=None, error_data=None):
+		self.message = message
+		self.code = code
+		self.error_data = error_data
+
+		if self.code:
+			message = '[{}] {}'.format(self.code, self.message)
+
+		super(ScupError, self).__init__(message)
+
+class HTTPError(ScupPythonError):
+	""" Exception for transport errors. """
